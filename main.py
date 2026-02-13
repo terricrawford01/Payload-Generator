ba8d5dc2ac96da9c19d23272913c95332c702127
terricrawford01 <terri.crawford@selu.edu>
2026-02-13 16:01:57 -0600

    Add initial implementation of fuzz generator for buffer overflow testing

---
+++ main.py
@@ -0,0 +1,23 @@
+user_input = input("What is your base string?")
+
+def fuzz_generator(target_input):
+    
+    fuzz_vectors = []
+    payload_list = []
+
+    bad_char = (input(f"What character would you like to use for your buffer overflow? ")) 
+    count = int(input("Enter the number of bufffer text you would like included: "))
+
+    for i in range(1, count + 1): 
+        overflow = bad_char * (i * 2)
+        fuzz_vectors.append(overflow) 
+
+    for vector in fuzz_vectors:
+        mutated_string = f"{target_input}{vector}"
+        payload_list.append(mutated_string)  
+    
+    return (payload_list)
+
+my_payloads = (fuzz_generator(user_input))
+print (len(my_payloads))
+print (my_payloads)
